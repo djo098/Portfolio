6d850749e0a933fd4142c95da3499e9898a6546f
Djo98 <mateuscdiogo@gmail.com>
2023-10-04 18:07:22 +0100

    Init projects section

--- src/components/Projetos.js
+++ src/components/Projetos.js
@@ -1,12 +1,53 @@
-import React from "react";
-import '../styles/Projects.scss';
+import React, { useEffect, useState } from "react";
+import { ReactComponent as LineSvg } from "../images/50px line.svg";
+import "../styles/Projects.scss";
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import OpenInNewRoundedIcon from '@mui/icons-material/OpenInNewRounded';
 
 const Projetos = () => {
-    return(
-        <div className="projects">
-            gangang;
-        </div>
+  const [user, setUser] = useState([]);
+
+  const fetchRepos = () => {
+    fetch(`https://api.github.com/users/djo098/repos`)
+      .then((response) => response.json())
+      .then((data) => {
+        setUser(data);
+      });
+  };
+
+  useEffect(() => {
+    fetchRepos();
+  }, []);
+
+  const userElements = user.map((userElement) => {
+    return (
+      <div className="repo-card" key={userElement.id}>
+        <p className="repo-name">{userElement.name}</p>
+        <p className="description">{userElement.description}</p>
+        <Stack direction="row" spacing={2}>
+            <Button className="mui-button" href={userElement.html_url} target="_blank" rel="noreferrer">
+                <GitHubIcon />
+            </Button>
+            <Button className="mui-button disabled" disabled href={userElement.html_url} target="_blank" rel="noreferrer">
+                <OpenInNewRoundedIcon />
+            </Button>
+        </Stack>
+      </div>
     );
-}
+  });
+
+  return (
+    <div className="projects">
+      <div className="container">
+        <div className="header">
+          <LineSvg className="Svg1" /> <h1>&#60;Projetos /&#62;</h1>
+        </div>
+        {userElements}
+      </div>
+    </div>
+  );
+};
 
 export default Projetos;
